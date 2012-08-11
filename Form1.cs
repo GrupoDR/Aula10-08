8b48825f7375366d8d27e01c8aad0ab0108b7238
Diego Germano <diego_germano28@yahoo.com.br>
2012-08-10 23:47:19 -0300

    Aula 10/08 esta adicionando e apagando do ListBox

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace aula10_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove (listBox1.SelectedItem);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string teste = textBox1.Text;
+
+            listBox1.Items.Add(teste);
+        }
+    }
+}
