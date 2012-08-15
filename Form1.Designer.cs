e327c6fe84790758d30c6392a95c5732db67fcac
Diego Germano <diego_germano28@yahoo.com.br>
2012-08-14 21:19:36 -0300

    Fiz uma alteracao nada ve

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.TEXTO = new System.Windows.Forms.Label();
-            this.Oquedeveadicionar = new System.Windows.Forms.TextBox();
-            this.Adicionar = new System.Windows.Forms.Button();
-            this.Apagar = new System.Windows.Forms.Button();
-            this.Lista_da_caixa = new System.Windows.Forms.ListBox();
+            this.AddDados = new System.Windows.Forms.TextBox();
+            this.B_ADD = new System.Windows.Forms.Button();
+            this.B_DEL = new System.Windows.Forms.Button();
+            this.Caixa_de_lista = new System.Windows.Forms.ListBox();
             this.COMFLITO_SE_VIRA_RODRIGAO = new System.Windows.Forms.GroupBox();
-            this.Fechar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.COMFLITO_SE_VIRA_RODRIGAO.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,49 +49,49 @@
             this.TEXTO.TabIndex = 0;
             this.TEXTO.Text = "PARA COMPRAR";
             // 
-            // Oquedeveadicionar
+            // AddDados
             // 
-            this.Oquedeveadicionar.Location = new System.Drawing.Point(6, 50);
-            this.Oquedeveadicionar.Name = "Oquedeveadicionar";
-            this.Oquedeveadicionar.Size = new System.Drawing.Size(421, 23);
-            this.Oquedeveadicionar.TabIndex = 1;
+            this.AddDados.Location = new System.Drawing.Point(6, 50);
+            this.AddDados.Name = "AddDados";
+            this.AddDados.Size = new System.Drawing.Size(421, 23);
+            this.AddDados.TabIndex = 1;
             // 
-            // Adicionar
+            // B_ADD
             // 
-            this.Adicionar.Location = new System.Drawing.Point(514, 50);
-            this.Adicionar.Name = "Adicionar";
-            this.Adicionar.Size = new System.Drawing.Size(174, 32);
-            this.Adicionar.TabIndex = 2;
-            this.Adicionar.Text = "&ADD";
-            this.Adicionar.UseVisualStyleBackColor = true;
-            this.Adicionar.Click += new System.EventHandler(this.button1_Click);
+            this.B_ADD.Location = new System.Drawing.Point(514, 50);
+            this.B_ADD.Name = "B_ADD";
+            this.B_ADD.Size = new System.Drawing.Size(174, 68);
+            this.B_ADD.TabIndex = 2;
+            this.B_ADD.Text = "&ADD";
+            this.B_ADD.UseVisualStyleBackColor = true;
+            this.B_ADD.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Apagar
+            // B_DEL
             // 
-            this.Apagar.Location = new System.Drawing.Point(514, 108);
-            this.Apagar.Name = "Apagar";
-            this.Apagar.Size = new System.Drawing.Size(174, 34);
-            this.Apagar.TabIndex = 3;
-            this.Apagar.Text = "&DELETE";
-            this.Apagar.UseVisualStyleBackColor = true;
-            this.Apagar.Click += new System.EventHandler(this.button2_Click);
+            this.B_DEL.Location = new System.Drawing.Point(514, 187);
+            this.B_DEL.Name = "B_DEL";
+            this.B_DEL.Size = new System.Drawing.Size(174, 66);
+            this.B_DEL.TabIndex = 3;
+            this.B_DEL.Text = "&DELETE";
+            this.B_DEL.UseVisualStyleBackColor = true;
+            this.B_DEL.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Lista_da_caixa
+            // Caixa_de_lista
             // 
-            this.Lista_da_caixa.FormattingEnabled = true;
-            this.Lista_da_caixa.ItemHeight = 16;
-            this.Lista_da_caixa.Location = new System.Drawing.Point(6, 108);
-            this.Lista_da_caixa.Name = "Lista_da_caixa";
-            this.Lista_da_caixa.Size = new System.Drawing.Size(421, 276);
-            this.Lista_da_caixa.TabIndex = 4;
+            this.Caixa_de_lista.FormattingEnabled = true;
+            this.Caixa_de_lista.ItemHeight = 16;
+            this.Caixa_de_lista.Location = new System.Drawing.Point(9, 118);
+            this.Caixa_de_lista.Name = "Caixa_de_lista";
+            this.Caixa_de_lista.Size = new System.Drawing.Size(421, 196);
+            this.Caixa_de_lista.TabIndex = 4;
             // 
             // COMFLITO_SE_VIRA_RODRIGAO
             // 
-            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Fechar);
-            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Apagar);
-            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Adicionar);
-            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Lista_da_caixa);
-            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Oquedeveadicionar);
+            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.button1);
+            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.B_DEL);
+            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.B_ADD);
+            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.Caixa_de_lista);
+            this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.AddDados);
             this.COMFLITO_SE_VIRA_RODRIGAO.Controls.Add(this.TEXTO);
             this.COMFLITO_SE_VIRA_RODRIGAO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.COMFLITO_SE_VIRA_RODRIGAO.Location = new System.Drawing.Point(7, 12);
@@ -99,15 +101,21 @@
             this.COMFLITO_SE_VIRA_RODRIGAO.TabStop = false;
             this.COMFLITO_SE_VIRA_RODRIGAO.Text = "TESTE GIT";
             // 
-            // Fechar
+            // button1
             // 
-            this.Fechar.Location = new System.Drawing.Point(514, 168);
-            this.Fechar.Name = "Fechar";
-            this.Fechar.Size = new System.Drawing.Size(174, 34);
-            this.Fechar.TabIndex = 5;
-            this.Fechar.Text = "&SAIR";
-            this.Fechar.UseVisualStyleBackColor = true;
-            this.Fechar.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.Location = new System.Drawing.Point(514, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&SAIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // Form1
             // 
@@ -128,12 +136,13 @@
         #endregion
 
         private System.Windows.Forms.Label TEXTO;
-        private System.Windows.Forms.TextBox Oquedeveadicionar;
-        private System.Windows.Forms.Button Adicionar;
-        private System.Windows.Forms.Button Apagar;
-        private System.Windows.Forms.ListBox Lista_da_caixa;
+        private System.Windows.Forms.TextBox AddDados;
+        private System.Windows.Forms.Button B_ADD;
+        private System.Windows.Forms.Button B_DEL;
+        private System.Windows.Forms.ListBox Caixa_de_lista;
         private System.Windows.Forms.GroupBox COMFLITO_SE_VIRA_RODRIGAO;
-        private System.Windows.Forms.Button Fechar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,13 +18,13 @@ namespace aula10_08
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Lista_da_caixa.Items.Remove (Lista_da_caixa.SelectedItem);
+            Caixa_de_lista.Items.Remove (Caixa_de_lista.SelectedItem);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             //string teste = textBox1.Text;
-            Lista_da_caixa.Items.Add(Oquedeveadicionar.Text);
+            Caixa_de_lista.Items.Add(AddDados.Text);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -33,7 +33,6 @@ namespace aula10_08
             {
                 e.Cancel = true;
             }
-            // Ops!!!
         }
 
         private void button1_Click_1(object sender, EventArgs e)
